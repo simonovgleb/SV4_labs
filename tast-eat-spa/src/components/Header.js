de2ea3ee3a8f2f675368403fec299a202784131c
Gleb <167199383+simonovgleb@users.noreply.github.com>
2025-01-19 16:16:01 +0300

    refacotr: adjust header component

--- tast-eat-spa/src/components/Header.js
+++ tast-eat-spa/src/components/Header.js
@@ -48,7 +48,57 @@ export class Header extends React.Component {
             </div>
           </div>
         </div>
-        {/* Остальная часть Header (burger panel, aside и т.п.) аналогично включается */}
+
+        <div className="burger__panel">
+          <div className="burger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <aside className="burger__content">
+          <img src="./static/LOGO.png" alt="logo" className="burger__logo" />
+          <div className="burger__btns">
+            <div className="burger__btn">
+              <img src="./static/header/home.png" className="burger__btn__img" alt="home" />
+              <a href="./index.html" className="lng-home">Home</a>
+            </div>
+            <div className="burger__btn">
+              <img src="./static/header/information.png" className="burger__btn__img" alt="about us" />
+              <a href="#mini__info" className="lng-about__us">About Us</a>
+            </div>
+            <div className="burger__btn">
+              <img src="./static/header/burger.png" className="burger__btn__img" alt="our menu" />
+              <a href="#dishes" className="lng-burger-menu">Our Menu</a>
+            </div>
+            <div className="burger__btn">
+              <img src="./static/header/web-browser.png" className="burger__btn__img" alt="pages" />
+              <a href="#" className="lng-burger-pages">Pages</a>
+            </div>
+            <div className="burger__btn">
+              <img src="./static/header/paper.png" className="burger__btn__img" alt="blog" />
+              <a href="#blog" className="lng-burger-blog">Blog</a>
+            </div>
+            <div className="burger__btn">
+              <img src="./static/header/contact-us.png" className="burger__btn__img" alt="contact us" />
+              <a href="#footer" className="lng-burger-contact">Contact Us</a>
+            </div>
+            <div className="burger__btn">
+              <img src="./static/header/phone.png" className="burger__btn__img" alt="call" />
+              <a href="tel:1234" className="lng-burger-tel">Call - 123 456 789</a>
+            </div>
+            <div className="burger__btn">
+              <a href="./#book-table" className="lng-burger-reservation">Reservation</a>
+            </div>
+          </div>
+          <div className="header__navbar__networks">
+            <img src="./static/inst.png" alt="social network" className="header__network" />
+            <img src="./static/facebook.png" alt="social network" className="header__network" />
+            <img src="./static/twitter.png" alt="social network" className="header__network" />
+            <img src="./static/pinterest.png" alt="social network" className="header__network" />
+          </div>
+        </aside>
       </header>
     );
   }
