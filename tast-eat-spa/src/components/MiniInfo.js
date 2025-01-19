5a0fe5c4545af51d8d6c8e8349c63d15f0570bb1
Gleb <167199383+simonovgleb@users.noreply.github.com>
2025-01-19 16:16:20 +0300

    refactor: adjust miniinfo component

--- tast-eat-spa/src/components/MiniInfo.js
+++ tast-eat-spa/src/components/MiniInfo.js
@@ -9,7 +9,11 @@ export class MiniInfo extends React.Component {
                 <div className="container__max">
                     <div className="mini__info__content">
                         <div className="mini__info__item">
-                            <img src="./static/geolocation.png" alt="icons" className="mini__info__item__image" />
+                            <img
+                                src="./static/geolocation.png"
+                                alt="icons"
+                                className="mini__info__item__image"
+                            />
                             <div className="mini__info__item__text">
                                 <p className="mini__info__item__title lng-mini__info__item__title__1">
                                     Locate Us
@@ -19,7 +23,38 @@ export class MiniInfo extends React.Component {
                                 </p>
                             </div>
                         </div>
-                        {/* Остальные элементы mini__info аналогично */}
+
+                        <div className="mini__info__item">
+                            <img
+                                src="./static/speed.png"
+                                alt="icons"
+                                className="mini__info__item__image"
+                            />
+                            <div className="mini__info__item__text">
+                                <p className="mini__info__item__title lng-mini__info__item__title__2">
+                                    Open Hours
+                                </p>
+                                <p className="mini__info__item__about lng-mini__info__item__about__2">
+                                    Mon To Fri 9:00 AM - 9:00 PM
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="mini__info__item">
+                            <img
+                                src="./static/notebook.png"
+                                alt="icons"
+                                className="mini__info__item__image"
+                            />
+                            <div className="mini__info__item__text">
+                                <p className="mini__info__item__title lng-mini__info__item__title__3">
+                                    Reservation
+                                </p>
+                                <p className="mini__info__item__about">
+                                    restaurantate@gmail.com
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
