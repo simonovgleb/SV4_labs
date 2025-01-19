7d411d1ab43b3b6b266202bbb93b2cfa4d29e9b3
Gleb <167199383+simonovgleb@users.noreply.github.com>
2025-01-19 16:19:10 +0300

    feat: add statics and styles from figma design layout

--- tast-eat-spa/src/App.js
+++ tast-eat-spa/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { Header } from './components/Header';
+import { Welcome } from './components/Welcome';
+import { MiniInfo } from './components/MiniInfo';
+import { Dishes } from './components/Dishes';
+import { Blog } from './components/Blog';
+import { Footer } from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="page__wrapper">
+        <Header />
+        <main className="content__of__page">
+          <Welcome />
+          <MiniInfo />
+          <Dishes />
+          <Blog />
+          {/* Другие секции по необходимости */}
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
